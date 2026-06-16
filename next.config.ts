99e46ddafb0c4ad6d792e0f294ca9fd5096a1d03
Ashish Chaubey <ashish.chaubey@neudhi23.com>
2026-06-16 17:27:12 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/maintenance',
-        permanent: false,
-      },
-      {
-        source: '/:path((?!maintenance|_next|favicon.ico).*)',
-        destination: '/maintenance',
-        permanent: false,
-      },
-    ]
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
